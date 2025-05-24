444c20ff1184f17dc8a6c3eeff7ea747fb507271
MMonu01 <mdmonu2021@gmail.com>
2025-05-24 21:05:41 +0530

    read a file

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const fs = require("fs/promises");
+const {Buffer} = require("node:buffer");
 
 (async () => {
+const  commandFileHandler = await fs.open("./command.txt")
+
   const watcher = fs.watch("./command.txt");
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
       console.log("something has changed");
-    }
+//	console.log(commandFileHandler)
+	// get the file size
+	const size  = (await commandFileHandler.stat()).size;
+
+	//buffer to store the data read
+	const buff =  Buffer.alloc(size);
+
+	const offset = 0
+	const length = buff.byteLength;
+	const position = 0
+	console.log(buff);
+	const content = await commandFileHandler.read(buff,offset,length,position);
+	console.log(content);
+  }
   }
 })();
