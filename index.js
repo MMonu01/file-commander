a7f5ae5f2c2b4cb51c1b3be188132cd5036d2d46
MMonu01 <mdmonu2021@gmail.com>
2025-05-24 21:19:54 +0530

    formatting

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const fs = require("fs/promises");
-const {Buffer} = require("node:buffer");
+const { Buffer } = require("node:buffer");
 
 (async () => {
-const  commandFileHandler = await fs.open("./command.txt")
-
   const watcher = fs.watch("./command.txt");
+
+  const commandFileHandler = await fs.open("./command.txt");
+
+  commandFileHandler.on("change", async () => {
+    // get the size of our file
+    const size = (await commandFileHandler.stat()).size;
+
+    //buffer to fill the data from file with the size of the file
+    const buff = Buffer.alloc(size);
+
+    //starting posisiton of buffer to fill our buffer
+    const offset = 0;
+
+    //how many bytes we want to read
+    const length = buff.byteLength;
+
+    // position from which to start reading the file
+    const position = 0;
+
+    const content = await commandFileHandler.read(
+      buff,
+      offset,
+      length,
+      position
+    );
+    console.log(content);
+  });
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      console.log("something has changed");
-//	console.log(commandFileHandler)
-	// get the file size
-	const size  = (await commandFileHandler.stat()).size;
-
-	//buffer to store the data read
-	const buff =  Buffer.alloc(size);
-
-	const offset = 0
-	const length = buff.byteLength;
-	const position = 0
-	console.log(buff);
-	const content = await commandFileHandler.read(buff,offset,length,position);
-	console.log(content);
-  }
+      commandFileHandler.emit("change");
+    }
   }
 })();
